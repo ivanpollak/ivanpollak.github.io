fa0c02c9edb78d9828a1d627cb05c61ae1c653a7
ivanpollak <ivanpollak@tutanota.com>
2024-11-14 16:52:59 +0100

    Updates
    - Better project entries

--- src/components/ProjectEntry/ProjectEntry.js
+++ src/components/ProjectEntry/ProjectEntry.js
@@ -1,16 +1,39 @@
-import "./ProjectEntry.css"
+import React, { useState } from "react";
+import "./ProjectEntry.css";
 
-function ProjectEntry({title, description}){
-    return(
-        <div className="ProjectEntryDiv">
-        <h2>
-            {title}
-        </h2>
-        <div>
-            {description}
+function ProjectEntry({ title, description, shortDesc }) {
+    // Zustand für das Umschalten der Beschreibung
+    const [showDescription, setShowDescription] = useState(false);
+
+    // Funktion zum Umschalten
+    const showFullDescription = (event) => {
+        event.stopPropagation(); // Verhindert das Event-Bubbling
+        setShowDescription(true);
+
+    };
+
+    const hideFullDescription = (event) => {
+        event.stopPropagation(); // Verhindert das Event-Bubbling
+        setShowDescription(false);
+    };
+
+    return (
+        <div className="ProjectEntryDiv" onClick={showFullDescription} style={showDescription ? { cursor: "auto" } : { cursor: "pointer" }}
+>
+            <h2>{title}</h2>
+            <div>
+                {shortDesc}
+                {showDescription && (
+                    <p className="description">{description}</p>
+                )}
+            </div>
+            {showDescription && (
+                <div className="hideButton" onClick={hideFullDescription}>
+                    Hide description
+                </div>
+            )}
         </div>
-    </div>
-    )
+    );
 }
 
 export default ProjectEntry;
--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -12,60 +12,123 @@ function Projects() {
             <h1>
                 Projects
             </h1>
+            <div className="infoBarProjects">
+                Click on a project to expand it.
+            </div>
             <div className="ProjectsListDiv">
 
                 <ProjectEntry
                     title={"ExoDist"}
-                    description={
+                    shortDesc={
                         <div>
-                            <h3>
-                                Description
-                            </h3>
                             <p>
-                                <i> ExoDist </i> is an distortion audio plugin.
-                                It uses a soft clipper algorythm i wrote completely on my own.
-                                This whole plugin is developed using C++ and the JUCE Framework.
-                            </p>
-                            <Link to={"https://github.com"} target="blank">
-                                link
-                            </Link>
-                        </div>
-                    }/>
-                
-                <ProjectEntry
-                    title={"Web Scraper"}
-                    description={
-                        <div>
-                            <h3>
-                                Description
-                            </h3>
-                            <p>
-                                A simple webscraper, that can be automated within the app.
-                                Coded using C# and WPF using the Selenium Framework for webscraping.
-                            </p>
-                            <p>
-                                No link since this is a private project.
+                                <b>ExoDist </b> is a custom-designed distortion audio plugin,
+                                inspired by my passion for music production and DJing.
                             </p>
                         </div>
-                    }/>
-                
-                <ProjectEntry
-                    title={"Portfolio Website"}
+                    }
                     description={
                         <div>
+                            <p>
+                                From the moment i started creating music,
+                                I dreamed of building my own audio plugin that would fit my exact needs,
+                                streamline my workflow,
+                                and allow me to dive deep into the world of audio processing.
+                                I was fascinated by how mathematics and signal processing can be used
+                                to shape sound, even though the complexity of thes field was intimidating.
+                            </p>
+
                             <h3>
-                                Description
+                                The Journey
                             </h3>
                             <p>
-                                This website you are currently looking at :)
+                                Developing ExoDist was a challenging yet rewarding journey.
+                                Initially, the biggest hurdle was the choice of programming language:
+                                C++.
+                                Althrough it's known for its efficiency and its a go-to for audio plugins,
+                                I had no prior experience with it.
+                                After some struggles with tutorials and limited time outside of work,
+                                I decidedt to make a bold move.
+                                I asked my trainer for permission to dedicate work hours to this project,
+                                and he agreed.
+                                Wit this support, I was able to dive headfirst into learning C++,
+                                the JUCE framework, and the fundamentals of Digital Signal Progessing (DSP). 
                             </p>
+
+                            <h3>
+                                Development Process
+                            </h3>
                             <p>
-                                Coded using HTML, CSS and JS with the React Framework.
-                                Using Github Pages for the deployment.
+                                I kicked off the project usint the <b>IPERKA method</b>,
+                                a structured approach taught in Swiss IT schools.
+                                This helped me outline my vision, set clear goals, 
+                                and start coding with a solid plan.
+                                As i delved deeper into the project,
+                                it quickly became apparent that this was no small task - 
+                                it would take several months to complete.
+                                Despite the challanges, I've made significant progress and
+                                am now in the final stages of development, 
+                                with most of the DSP work complete.
+                                The UI is the next major milestone before release.
                             </p>
-                            <Link to={"https://github.com"} target="blank">
-                                link
-                            </Link>
+
+                            <h3>
+                                Key Learnings
+                            </h3>
+                            <p>
+                                Working on ExoDist has been an incredible learning experience,
+                                pushing me far beyond my comfort zone.
+                                Here are some of the skills and technologies I mastered along the way:
+                            </p>
+                            <ul>
+                                <li>
+                                    <b>Digital Signal Processing (DSP)</b>:
+                                    Understanding the intricacies of audio processing, including IIR and FIR filters.
+                                </li>
+
+                                <li>
+                                    <b>Algorithm Design and Optimization</b>:
+                                    Developing and fine-tuning my own algorithm for real-time audio processing.
+                                </li>
+
+                                <li>
+                                    <b>C++ programming</b>:
+                                    Gaining proficiency in C++, 
+                                    including memory management, 
+                                    pointers, 
+                                    and efficient code practices.
+                                </li>
+
+                                <li>
+                                    <b>Plugin Development</b>:
+                                    VST3 and exploring various plugin technologies.
+                                </li>
+
+                                <li>
+                                    <b>UI Design</b>:
+                                    Learning how to create intuitive and visually appealing interfaces for audio plugins.
+                                </li>
+
+                                <li>
+                                    <b>Advanced Audio Effects</b>
+                                    Exploring non-linear functions and other effects like delay, reverb, and oscillators.
+                                </li>
+                            </ul>
+
+                            <h3>
+                                What's Next?
+                            </h3>
+
+                            <p>
+                            ExoDist is still a work in progress, 
+                            but the end is in sight. 
+                            The source code is available as an open project, 
+                            while compiled binaries will be available for purchase upon release. 
+                            This project has not only taught me about the technical 
+                            side of audio plugin development 
+                            but has also shown me how deeply rewarding it is 
+                            to create a tool that bridges my love for coding and music production.
+                            </p>
                         </div>
                     }/>
             </div>
